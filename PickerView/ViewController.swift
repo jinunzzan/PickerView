c24eac51c6ef821533082e952d533a398feb46e7
jinunzzan <lovebox6607@gmail.com>
2023-01-02 14:43:07 +0900

    아울렛 선언

--- PickerView/ViewController.swift
+++ PickerView/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pickerImage: UIPickerView!
+    @IBOutlet weak var lblImageFileName: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
